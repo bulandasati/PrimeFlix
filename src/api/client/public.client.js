7be68608449ec3f163a7006bef92bb0002d796e1
bulandasati <bulandasati06@gmail.com>
2024-09-21 13:29:25 +0530

    DB connected

--- src/api/client/public.client.js
+++ src/api/client/public.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "https://ashuflix-server.onrender.com/api/v1/";
+const baseURL = "https://streamflix-server-578m.onrender.com/api/v1/";
 
 const publicClient = axios.create({
   baseURL,
